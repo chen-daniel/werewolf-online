bf11e1715037a3ef76ee633622dca7f223961701
chen-daniel <chen.daniel@husky.neu.edu>
2020-08-07 13:55:44 -0400

    Add endpoint for creating a room

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000
+app.set('port', port);
+
+const roomCodeLength = 4;
+const roomCodeChars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+let rooms = {};
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+function generateRoomCode() {
+  result = '';
+  do {
+    for (let i = 0; i < roomCodeLength; i++) {
+      result += roomCodeChars[Math.floor(Math.random() * roomCodeChars.length)];
+    }
+  } while (rooms[result])
+  return result;
+}
+
+function Game() {
+  this.deck = [];
+  this.players = [];
+  this.gameState = {};
+}
+
+app.get('/create-game', (req, res) => {
+  const roomCode = generateRoomCode();
+  rooms[roomCode] = new Game();
+  res.json({room: roomCode})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 })
