f4d7a215e420a5c27eb088b628659768e525e499
chen-daniel <chen.daniel@husky.neu.edu>
2020-08-08 07:15:36 -0400

    Updates

---
+++ precache-manifest.1f50fc70aebf5812c7d3eeee96e81d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d64596ea01569bb161e12e2e26b673",
+    "url": "/werewolf-online/index.html"
+  },
+  {
+    "revision": "ed3e3ca84b7ba575cbbf",
+    "url": "/werewolf-online/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "575df580fad82754d7e7",
+    "url": "/werewolf-online/static/js/2.d83f385a.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/werewolf-online/static/js/2.d83f385a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3e3ca84b7ba575cbbf",
+    "url": "/werewolf-online/static/js/main.4054e92b.chunk.js"
+  },
+  {
+    "revision": "60eb1a453bd765b6369f",
+    "url": "/werewolf-online/static/js/runtime-main.69d01ce0.js"
+  }
+]);
