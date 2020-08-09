bb9173d82a7239115533c1ee213b9cde46fd0005
chen-daniel <chen.daniel@husky.neu.edu>
2020-08-09 09:35:37 -0400

    update heroku app url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 
 
 module.exports = {
-    api: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://still-oasis-65835.herokuapp.com'
+    api: process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://shrouded-plateau-06981.herokuapp.com'
 }
